af192df0503b31cd4b4d7d8dc54d71944a4aeca7
avalojandro <avalojandro@gmail.com>
2020-03-08 20:05:34 -0600

    BETA

--- X Public/src/app.js
+++ X Public/src/app.js
@@ -1,20 +1,8 @@
-import {X_Sum, X_subtraction, X_multiplication, X_division} from '../xpace';
+import {X_sumF,X_DivisionF,X_Root} from '../xpace';
 
-    const NuevaSuma = new X_Sum(10,4,6,9,2);
-    let result1 = NuevaSuma.Sum5();
-    console.log(result1);
 
-    const NuevaResta = new X_subtraction(10,4,6,9,2);
-    let result2 = NuevaResta.Subs5();
-    console.log(result2);
-    
-    const  NuevaMultiplicacion = new X_multiplication(10,4,6,9,2);
-    let result3 = NuevaMultiplicacion.Multi5();
-    console.log(result3);
+const raizCuadrada = new X_Root(100);
+let raiz = raizCuadrada.FifthRoot();
+console.log(raiz);
 
-    const  NuevaDivision = new X_division(10,4,6,9,2);
-    let result4 = NuevaDivision.Divi5();
-    console.log(result4);
 
-    
-   
--- X Public/xpace.js
+++ X Public/xpace.js
@@ -1,5 +1,9 @@
 //Xpace JS with all the functions in the same file
 
+
+//MATH BASICS
+
+//Sum
 export class X_Sum
 {
     constructor(num1 = 0,num2 = 0,num3 = 0,num4 = 0,num5 = 0)
@@ -22,7 +26,7 @@ export class X_Sum
     }
     //add four numbers
     Sum4(){
-        const result = parseInt(this.num1 + this.num2 + this.num3 + this.num4 + this.num5);
+        const result = parseInt(this.num1 + this.num2 + this.num3 + this.num4);
         return result;
     }
     //add five numbers
@@ -33,7 +37,8 @@ export class X_Sum
 
 }
 
-export class X_subtraction
+//Substraction
+export class X_Subtraction
 {
     constructor(num1 = 0,num2 = 0,num3 = 0,num4 = 0,num5 = 0)
     {
@@ -55,7 +60,7 @@ export class X_subtraction
     }
     //add four numbers
     Subs4(){
-        const result = parseInt(this.num1 - this.num2 - this.num3 - this.num4 - this.num5);
+        const result = parseInt(this.num1 - this.num2 - this.num3 - this.num4);
         return result;
     }
     //add five numbers
@@ -66,7 +71,8 @@ export class X_subtraction
 
 }
 
-export class X_multiplication
+//Multiplication
+export class X_Multiplication
 {
     constructor(num1 = 0,num2 = 0,num3 = 0,num4 = 0,num5 = 0)
     {
@@ -88,7 +94,7 @@ export class X_multiplication
     }
     //add four numbers
     Multi4(){
-        const result = parseInt(this.num1 * this.num2 * this.num3 * this.num4 * this.num5);
+        const result = parseInt(this.num1 * this.num2 * this.num3 * this.num4);
         return result;
     }
     //add five numbers
@@ -99,7 +105,8 @@ export class X_multiplication
 
 }
 
-export class X_division
+//Division
+export class X_Division
 {
     constructor(num1 = 0,num2 = 0,num3 = 0,num4 = 0,num5 = 0)
     {
@@ -121,7 +128,7 @@ export class X_division
     }
     //add four numbers
     Divi4(){
-        const result = parseInt(this.num1 / this.num2 / this.num3 / this.num4 / this.num5);
+        const result = parseInt(this.num1 / this.num2 / this.num3 / this.num4);
         return result;
     }
     //add five numbers
@@ -130,5 +137,210 @@ export class X_division
         return result;
     }
 
+}
+
+//MATH BASICS (FLOAT)
+
+//Sum Float
+export class X_SumF
+{
+    constructor(num1 = 0,num2 = 0,num3 = 0,num4 = 0,num5 = 0)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.num4 = num4;
+        this.num5 = num5;
+    }
+    //add two numbers
+    Sum2(){
+        const result = parseFloat(this.num1 + this.num2);
+        return result;
+    }
+    //add three numbers
+    Sum3(){
+        const result = parseFloat(this.num1 + this.num2 + this.num3);
+        return result;
+    }
+    //add four numbers
+    Sum4(){
+        const result = parseFloat(this.num1 + this.num2 + this.num3 + this.num4);
+        return result;
+    }
+    //add five numbers
+    Sum5(){
+        const result = parseFloat(this.num1 + this.num2 + this.num3 + this.num4 + this.num5);
+        return result;
+    }
+
+}
+
+//Substraction Float
+export class X_SubstractionF
+{
+    constructor(num1 = 0,num2 = 0,num3 = 0,num4 = 0,num5 = 0)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.num4 = num4;
+        this.num5 = num5;
+    }
+    //add two numbers
+    Subs2(){
+        const result = parseFloat(this.num1 - this.num2);
+        return result;
+    }
+    //add three numbers
+    Subs3(){
+        const result = parseFloat(this.num1 - this.num2 - this.num3);
+        return result;
+    }
+    //add four numbers
+    Subs4(){
+        const result = parseFloat(this.num1 - this.num2 - this.num3 - this.num4);
+        return result;
+    }
+    //add five numbers
+    Subs5(){
+        const result = parseFloat(this.num1 - this.num2 - this.num3 - this.num4 - this.num5);
+        return result;
+    }
+
+}
+
+//Multiplication Float
+export class X_MultiplicationF
+{
+    constructor(num1 = 0,num2 = 0,num3 = 0,num4 = 0,num5 = 0)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.num4 = num4;
+        this.num5 = num5;
+    }
+    //add two numbers
+    Multi2(){
+        const result = parseFloat(this.num1 * this.num2);
+        return result;
+    }
+    //add three numbers
+    Multi3(){
+        const result = parseFloat(this.num1 * this.num2 * this.num3);
+        return result;
+    }
+    //add four numbers
+    Multi4(){
+        const result = parseFloat(this.num1 * this.num2 * this.num3 * this.num4);
+        return result;
+    }
+    //add five numbers
+    Multi5(){
+        const result = parseFloat(this.num1 * this.num2 * this.num3 * this.num4 * this.num5);
+        return result;
+    }
+
+}
+
+//Division Float
+export class X_DivisionF
+{
+    constructor(num1 = 0,num2 = 0,num3 = 0,num4 = 0,num5 = 0)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.num4 = num4;
+        this.num5 = num5;
+    }
+    //add two numbers
+    Divi2(){
+        const result = parseFloat(this.num1 / this.num2);
+        return result;
+    }
+    //add three numbers
+    Divi3(){
+        const result = parseFloat(this.num1 / this.num2 / this.num3);
+        return result;
+    }
+    //add four numbers
+    Divi4(){
+        const result = parseFloat(this.num1 / this.num2 / this.num3 / this.num4);
+        return result;
+    }
+    //add five numbers
+    Divi5(){
+        const result = parseFloat(this.num1 / this.num2 / this.num3 / this.num4 / this.num5);
+        return result;
+    }
+
+}
+
+//ROOTS
+export class X_Root
+{
+    constructor(number) {this.number = number;}
+
+    SquareRoot()
+    {
+       const root = Math.pow(this.number,(1 / 2));
+       return root;
+    }
+
+    CubeRoot()
+    {
+       const root = Math.pow(this.number,(1 / 3));
+       return root;
+    }
+
+    FourthRoot()
+    {
+        const root = Math.pow(this.number,(1 / 4));
+        return root;
+    }
+
+    FifthRoot()
+    {
+        const root = Math.pow(this.number,(1 / 5));
+        return root;
+    }
+}   
+
+//AREAS
+export class X_Area
+{
+    constructor(base,height,radius,DM,Dm) 
+    {
+        this.base = base;
+        this.height = height;
+        this.radius = radius;
+        this.DM = DM;
+        this.Dm = Dm;
+    }
+
+    SquareArea()
+    {
+       const root = Math.pow(this.number,(1 / 2));
+       return root;
+    }
+
+    CircleArea()
+    {
+       const root = Math.pow(this.number,(1 / 3));
+       return root;
+    }
+
+    DiamondArea()
+    {
+        const root = Math.pow(this.number,(1 / 4));
+        return root;
+    }
+
+    TriangleArea()
+    {
+        const root = Math.pow(this.number,(1 / 5));
+        return root;
+    }
 }
 
